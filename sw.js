bafd4b69a9e6ef4a5dde191c912b30c810fc16f9
Nazanin <166415421+nazanin-ghaemizadeh@users.noreply.github.com>
2026-09-09 11:15:41 +0330

    Add BAMCO service worker

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'bamco-app-center-v1';
+const APP_SHELL = [
+  '/BAMCO-APP-CENTER/',
+  '/BAMCO-APP-CENTER/index.html',
+  '/BAMCO-APP-CENTER/manifest.webmanifest',
+  '/BAMCO-APP-CENTER/bamco-logo.png',
+  '/BAMCO-APP-CENTER/favicon.svg',
+  '/BAMCO-APP-CENTER/icons/task-management.svg',
+  '/BAMCO-APP-CENTER/icons/vehicle-assessment.svg',
+  '/BAMCO-APP-CENTER/icons/homologation.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(cached => cached || caches.match('/BAMCO-APP-CENTER/')))
+  );
+});
