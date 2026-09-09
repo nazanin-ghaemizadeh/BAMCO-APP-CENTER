5b58d256cf6577d4011581cc781521a44b1280fe
Nazanin <166415421+nazanin-ghaemizadeh@users.noreply.github.com>
2026-09-09 21:22:43 +0330

    Use supplied BAC app icons, simplify footer and compact portal layout

--- sw.js
+++ sw.js
@@ -1,10 +1,16 @@
-const CACHE_NAME = 'bamco-app-center-v11';
+const CACHE_NAME = 'bamco-app-center-v12';
 const APP_SHELL = [
   '/BAMCO-APP-CENTER/',
   '/BAMCO-APP-CENTER/index.html',
   '/BAMCO-APP-CENTER/manifest.webmanifest',
   '/BAMCO-APP-CENTER/bamco-logo-official.png',
-  '/BAMCO-APP-CENTER/favicon.svg',
+  '/BAMCO-APP-CENTER/icons/bac-favicon-32.png',
+  '/BAMCO-APP-CENTER/icons/bac-favicon-16.png',
+  '/BAMCO-APP-CENTER/icons/bac-favicon.ico',
+  '/BAMCO-APP-CENTER/icons/bac-apple-touch-180.png',
+  '/BAMCO-APP-CENTER/icons/bac-app-192.png',
+  '/BAMCO-APP-CENTER/icons/bac-app-512.png',
+  '/BAMCO-APP-CENTER/icons/bac-maskable-512.png',
   '/BAMCO-APP-CENTER/fonts/Vazirmatn-Regular.woff2',
   '/BAMCO-APP-CENTER/fonts/Vazirmatn-Bold.woff2'
 ];
